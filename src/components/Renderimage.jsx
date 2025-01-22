9fb96dec969d589d1fe351213fd4fd4b2077066f
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-22 14:17:35 +0530

    Update Renderimage.jsx

--- src/components/Renderimage.jsx
+++ src/components/Renderimage.jsx
@@ -46,9 +46,9 @@ export function Renderimage({ userName, Content }) {
   };
 
   return (
-    <div className="w-full flex flex-col justify-center items-center  ">
+    <div className="w-full flex flex-col justify-center items-center ">
       {/* Capture Area */}
-      <div id="capture-area" className="flex flex-col items-center justify-center  z-20   bg-black w-[300px] md:w-[350px] h-fit">
+      <div id="capture-area" className="flex flex-col items-center justify-center  z-20   bg-black w-[350px] md:w-[350px]">
         <Image width={2200} height={2200}  src={Group} alt="Mid Image" className="rounded-lg object-cover lg:w-[350px]" />
 
         <div className="text-white relative -top-16  bg-gradient-to-br from-yellowBackground to-yellowSecondary p-2 w-[330px] rounded-3xl border-white border-2 flex flex-col">
