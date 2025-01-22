6c2cce9afeb5f4f9d0d707737f1736cffcb70768
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-22 14:59:04 +0530

    Update Renderimage.jsx

--- src/components/Renderimage.jsx
+++ src/components/Renderimage.jsx
@@ -49,13 +49,13 @@ export function Renderimage({ userName, Content }) {
   return (
     <div className="w-full flex flex-col justify-center items-center ">
       {/* Capture Area */}
-      <div id="capture-area" className="flex flex-col items-center justify-center  z-20 my-2   bg-black w-[300px] md:w-[350px]">
+      <div id="capture-area" className="flex flex-col items-center justify-center  z-20 my-2  h-screen bg-black w-[300px] md:w-[350px]">
         <Image width={2200} height={2200}  src={Group} alt="Mid Image" className="rounded-lg object-cover w-[300px] lg:w-[350px]" />
 
-        <div className="text-white relative -top-16  bg-gradient-to-br from-yellowBackground to-yellowSecondary p-2 w-[330px] rounded-3xl border-white border-2 flex flex-col">
+        <div className="text-white relative  bg-gradient-to-br from-yellowBackground to-yellowSecondary p-2 w-[280px] rounded-3xl border-white border-2 flex flex-col">
         <p className="w-full text-center text-2xl font-myFont px-10 py-2 bg-lime-900 rounded-full flex justify-center items-center">
-  {userName}
-</p>
+      {userName}
+        </p>
 
           <p className="text text-black text-center font-myFont py-8">{Content}</p>
           <p className="text-center text-black font-myFont">Get yours at canditapp.com</p>
