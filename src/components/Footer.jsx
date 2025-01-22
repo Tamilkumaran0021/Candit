f64c8d180a3f4d068d6a2a28762d9390e712e5f1
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-22 10:35:45 +0530

    text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export function  Footer(){
         <>
             <div className='flex flex-col items-center py-20 '>
               <h1 className='text-yellowBackground text-4xl font-myFont pb-5 mt-28'>Join the fun!</h1>
-              <p className="py-4 font-bold">👇🏻 24 people just tapped the button 👇🏻</p>
+              <p className="py-4 font-bold text-white">👇🏻 24 people just tapped the button 👇🏻</p>
               <button className="bg-yellowBackground px-20 py-4 text-black font-black rounded-full text-lg ">Pre-book Candit</button>
           </div>
         </>
