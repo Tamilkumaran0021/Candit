3fc06512a5ec9eedced044feab9ac93435a46f06
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-23 09:59:27 +0530

    last code

--- src/components/Renderimage.jsx
+++ src/components/Renderimage.jsx
@@ -52,7 +52,7 @@ export function Renderimage({ userName, Content }) {
       <div id="capture-area" className="flex flex-col items-center justify-center  z-20 my-2  h-full bg-black w-full md:w-[350px]">
         <Image width={2200} height={2200}  src={Group} alt="Mid Image" className="rounded-lg object-cover w-[300px] lg:w-[350px]" />
 
-        <div className="text-white relative  bg-gradient-to-br from-yellowBackground to-yellowSecondary p-2 w-[280px] rounded-3xl border-white border-2 flex flex-col">
+        <div className="text-white relative -top-20  bg-gradient-to-br from-yellowBackground to-yellowSecondary p-2 w-[280px] rounded-3xl border-white border-2 flex flex-col">
         <p className="w-full text-center text-2xl font-myFont px-10 py-2 bg-lime-900 rounded-full flex justify-center items-center">
       {userName}
         </p>
