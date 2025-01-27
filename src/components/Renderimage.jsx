a622e18ce268b316686e8ca4e148046d6f3934b0
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-27 14:48:23 +0530

    doo

--- src/components/Renderimage.jsx
+++ src/components/Renderimage.jsx
@@ -81,11 +81,11 @@ export function Renderimage({ userName, Content }) {
         />
 
         <div className="text-white relative -top-20 bg-gradient-to-br from-yellowBackground to-yellowSecondary p-4 w-[280px] rounded-3xl border-white border-2 flex flex-col">
-         { (!screenstate)?(<div className="w-full text-center text-2xl font-myFont px-10 bg-lime-900 rounded-full flex justify-center items-center whitespace-nowrap overflow-hidden text-ellipsis">
-            <p className="w-full text-center m-auto " >{userName}</p>
+         { (!screenstate)?(<div className="w-full text-center text-2xl font-myFont px-10 bg-lime-900 rounded-full flex justify-center items-center ">
+            <p className=" " >{userName}</p>
           </div>):
-          (<div className="w-full text-center text-2xl font-myFont px-10 py-2 bg-lime-900 rounded-full  justify-center items-center whitespace-nowrap overflow-hidden text-ellipsis">
-            <p className="w-full text-center m-auto mt-0 mb-4" >{userName}</p>
+          (<div className="w-full text-center text-2xl font-myFont px-10 py-2 bg-lime-900 rounded-full  justify-center items-center">
+            <p className=" -mt-1 mb-4" >{userName}</p>
           </div>)}
 
           <p className="text-black text-center font-myFont py-8 tracking-wide leading-tight">
