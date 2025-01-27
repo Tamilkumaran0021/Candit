d2b770c0c0b5d59690a93711cf8c4e9645729ef2
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-01-27 14:18:01 +0530

    current

--- src/components/Renderimage.jsx
+++ src/components/Renderimage.jsx
@@ -79,7 +79,7 @@ export function Renderimage({ userName, Content }) {
 
         <div className="text-white relative -top-20 bg-gradient-to-br from-yellowBackground to-yellowSecondary p-4 w-[280px] rounded-3xl border-white border-2 flex flex-col">
           <div className="w-full text-center text-2xl font-myFont px-10 py-2 bg-lime-900 rounded-full flex justify-center items-center whitespace-nowrap overflow-hidden text-ellipsis">
-            <p className="w-full text-center">{userName}</p>
+            <p className="w-full text-center m-auto">{userName}</p>
           </div>
 
           <p className="text-black text-center font-myFont py-8 tracking-wide leading-tight">
